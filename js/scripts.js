5d9efbd4428aed35e56f4d8c44a44a2315d41c6a
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2018-01-06 20:50:11 +0100

    Add sticky to navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
-$('#toggle').click(function() {
+$('#js-toggle').click(function() {
     $(this).toggleClass('is-active');
-    $('#overlay').toggleClass('is-open');
+    $('#js-overlay').toggleClass('is-open');
    });
 
-$('#search-toggle').click(function() {
+$('#js-search-toggle').click(function() {
     $(this).toggleClass('is-active');
-    $('#search-overlay').toggleClass('is-open');
+    $('#js-search-overlay').toggleClass('is-open');
 });
+
+$(window).scroll(function(){
+    changeNavbar();
+});
+
+function changeNavbar() {
+    var home = $(document).scrollTop();
+    if (home >= 100) {
+        $('#js-nav').addClass('c-navigation__sticky');
+        $('#js-toggle').addClass('is-sticky');
+        $('#js-search-toggle').addClass('is-sticky');
+    } else {
+        $('#js-nav').removeClass('c-navigation__sticky');
+        $('#js-toggle').removeClass('is-sticky');
+        $('#js-search-toggle').removeClass('is-sticky');
+    }
+}
