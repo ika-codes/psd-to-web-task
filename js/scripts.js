4294171e6cafe2576a936002a66a02076d1d8867
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2018-01-07 11:44:23 +0100

    Fix sticky navbar, RWD display

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,41 @@
-$('#js-toggle').click(function() {
-    $(this).toggleClass('is-active');
-    $('#js-overlay').toggleClass('is-open');
-   });
+window.onload = function() {
 
-$('#js-search-toggle').click(function() {
-    $(this).toggleClass('is-active');
-    $('#js-search-overlay').toggleClass('is-open');
-});
+    $('#js-toggle').click(function() {
+        $(this).toggleClass('is-active');
+        $('#js-overlay').toggleClass('is-open');
+    });
 
-$(window).scroll(function(){
-    changeNavbar();
-});
+    $('#js-search-toggle').click(function() {
+        $(this).toggleClass('is-active');
+        $('#js-search-overlay').toggleClass('is-open');
+    });
 
-function changeNavbar() {
-    var home = $(document).scrollTop();
-    if (home >= 100) {
-        $('#js-nav').addClass('c-navigation__sticky');
-        $('#js-toggle').addClass('is-sticky');
-        $('#js-search-toggle').addClass('is-sticky');
-        $('#js-about-img').addClass('is-visible');
-    } else {
-        $('#js-nav').removeClass('c-navigation__sticky');
-        $('#js-toggle').removeClass('is-sticky');
-        $('#js-search-toggle').removeClass('is-sticky');
+    $(window).scroll(function(){
+        changeNavbar();
+    });
+
+    function changeNavbar() {
+        var home = $(document).scrollTop();
+        if (home >= 100) {
+            $('#js-nav').addClass('c-navigation__sticky');
+            $('#js-toggle').addClass('is-sticky');
+            $('#js-search-toggle').addClass('is-sticky');
+            $('#js-about-img').addClass('is-visible');
+        } else {
+            $('#js-nav').removeClass('c-navigation__sticky');
+            $('#js-toggle').removeClass('is-sticky');
+            $('#js-search-toggle').removeClass('is-sticky');
+        }
     }
-}
 
-var showText = function (target, message, index, interval) {    
-    if (index < message.length) { 
-      $(target).append(message[index++]); 
-      setTimeout(function () { showText(target, message, index, interval); }, interval); 
-    } 
-  }
-      
-  $(function () { 
-   
-    showText("#js-letter-header", "Lorem ipsum", 0, 100);  
-   
-  }); 
+    var showText = function (target, message, index, interval) {    
+        if (index < message.length) { 
+        $(target).append(message[index++]); 
+        setTimeout(function () { showText(target, message, index, interval); }, interval); 
+        } 
+    };
+        
+    $(function () { 
+        showText("#js-letter-header", "Lorem ipsum", 0, 100);  
+    }); 
+}
